e1f31fb8fb80385736585fc8b25fe3a36d659a32
Roh Geonho <roh517@naver.com>
2020-02-27 01:20:44 +0900

    listview
    card - image from network

--- minjok_herald/lib/pages/Home/Listview.dart
+++ minjok_herald/lib/pages/Home/Listview.dart
@@ -2,49 +2,135 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DocumentView extends StatelessWidget {
-
   final DocumentSnapshot documentData;
   DocumentView(this.documentData);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 3.0, vertical: 5.0),
-      height: 150,
+    if (documentData.data["url"] != null) {
+      return Container(
+          padding: EdgeInsets.symmetric(horizontal: 3.0, vertical: 5.0),
+          child: Card(
+            semanticContainer: true,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            color: Color.fromRGBO(255, 255, 255, 1.0),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(15),
+              onTap: () {
+                print('Card tapped.');
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.transparent,
+                ),
+                child: Stack(
+                  children: <Widget>[
+                    Image.network(
+                      documentData.data["url"],
+                      fit: BoxFit.fill,
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(10,2,10,10),
 
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        elevation: 3,
-
-        color: Color.fromRGBO(255, 255, 255, 1.0),
-//        decoration: BoxDecoration(
-//          border: Border.all(color: Colors.grey),
-//          borderRadius: BorderRadius.circular(5.0),
-//        ),
-        child: InkWell(
-        splashColor: Colors.blue.withAlpha(30),
-        onTap: () {
-        print('Card tapped.');
-        },
-        child: ListTile(
-          title: Text(
-              documentData.data["title"],
-              style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.w400),
-          ),
+                      title: Text(
+                        documentData.data["title"],
+                        style: TextStyle(
+                            fontSize: 17.5, fontWeight: FontWeight.w400),
+                      ),
 
 
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ));
+    } else {
+      return Container(
+          padding: EdgeInsets.symmetric(horizontal: 3.0, vertical: 5.0),
+          height: 150,
+          child: Card(
+            semanticContainer: true,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            color: Color.fromRGBO(255, 255, 255, 1.0),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(15),
+              onTap: () {
+                print('Card tapped.');
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.transparent,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(10,2,10,10),
 
-          subtitle: Row(
-            children: <Widget>[
-//              Text(documentData.data["price"].toString()),
-              SizedBox(width: 10, height: 10),
-//              Text(documentData.data["purchase?"].toString()),
-            ],
-          ),
-        ),
-      ),
-    ));
+                      title: Text(
+                        documentData.data["title"],
+                        style: TextStyle(
+                            fontSize: 17.5, fontWeight: FontWeight.w400),
+                      ),
+
+
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ));
+    }
   }
 }
+
+//      height: 150,
+
+//      child: Card(
+//        semanticContainer: true,
+//        clipBehavior: Clip.antiAliasWithSaveLayer,
+//
+//        shape: RoundedRectangleBorder(
+//          borderRadius: BorderRadius.circular(15),
+//        ),
+//        elevation: 3,
+//
+//        color: Color.fromRGBO(255, 255, 255, 1.0),
+//////        decoration: BoxDecoration(
+//////          border: Border.all(color: Colors.grey),
+//////          borderRadius: BorderRadius.circular(5.0),
+//////        ),
+//
+////        child: InkWell(
+////
+////        splashColor: Colors.blue.withAlpha(30),
+////        onTap: () {
+////        print('Card tapped.');
+////        },
+//        child: ListTile(
+//
+//          title: Text(
+//              documentData.data["title"],
+//              style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.w400),
+//          ),
+//
+//
+//          subtitle: Row(
+//            children: <Widget>[
+////              Text(documentData.data["price"].toString()),
+//              SizedBox(width: 10, height: 10),
+////              Text(documentData.data["purchase?"].toString()),
+//            ],
+//          ),
+//        ),
+//    )
