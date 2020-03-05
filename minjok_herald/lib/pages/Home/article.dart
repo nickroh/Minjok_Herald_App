0d6c03d3ef1b0f54a3ed2d7a864e6a232c80c312
Roh Geonho <roh517@naver.com>
2020-03-05 16:34:38 +0900

    comment

--- minjok_herald/lib/pages/Home/article.dart
+++ minjok_herald/lib/pages/Home/article.dart
@@ -1,48 +1,91 @@
-
-
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class articlepage extends StatefulWidget{
+class articlepage extends StatefulWidget {
   final DocumentSnapshot post;
 
   articlepage({this.post});
 
   @override
   articlepageState createState() => articlepageState();
-
 }
 
-class articlepageState extends State<articlepage>{
+class articlepageState extends State<articlepage> {
   String contents;
   DateTime posttime;
   String time;
+  bool comment_state = false;
+  String commenttmp = "";
+  String message = 'comment';
+  FocusNode myFocusNode;
+
+  final _formKey = GlobalKey<FormState>();
+
   var comments_data = new List();
 
-  void initState(){
+  void initState() {
     comments_data = widget.post.data['comments'];
     contents = widget.post.data["contents"];
     print(contents);
     contents = contents.replaceAll("\\n", "\n\n");
-
-    posttime = new DateTime.fromMillisecondsSinceEpoch(widget.post.data["timestamp"]);
+    myFocusNode = FocusNode();
+    posttime =
+        new DateTime.fromMillisecondsSinceEpoch(widget.post.data["timestamp"]);
     time = posttime.toString();
     int tmp;
-    for(int i=0;i<time.length;i++){
-      if(time[i]==" "){
+    for (int i = 0; i < time.length; i++) {
+      if (time[i] == " ") {
         tmp = i;
         break;
       }
     }
-    if(comments_data == null){
+    if (comments_data == null) {
       comments_data = [""];
     }
 
-    time = time.substring(0,tmp+1 );
+    time = time.substring(0, tmp + 1);
     print(time);
   }
 
+  TextEditingController _textFieldController = TextEditingController();
+  Widget write_comments() {
+    if (comment_state == false) {
+      return Container(
+          child: Padding(
+        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+      ));
+    } else {
+      return Container(
+          child: Form(
+        key: _formKey,
+        child: TextFormField(
+          focusNode: myFocusNode,
+          decoration: InputDecoration(
+              hintText: 'comments',
+              filled: false,
+              suffixIcon: IconButton(
+                  icon: Icon(Icons.check),
+                  onPressed: () {
+                    _onSave();
+                    debugPrint('222');
+                    comment_state = false;
+
+                    setState(() {});
+                  })),
+          validator: (value) {
+            commenttmp = value;
+            if (value.isEmpty) {
+              return '내용을 입력해주세요.';
+            }
+          },
+          onSaved: (val) => setState(() {
+//              widget.review.title = val;
+          }),
+        ),
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,86 +93,74 @@ class articlepageState extends State<articlepage>{
     return new Scaffold(
       body: SafeArea(
         child: CustomScrollView(
-              slivers: <Widget> [
-                SliverAppBar(
-                  expandedHeight: 180.0,
-                  floating: true,
-                  pinned: false,
-
-                  flexibleSpace: FlexibleSpaceBar(
-                    background: Hero(
-                        tag: widget.post.documentID,
-                        child: Image.network(
-                          widget.post.data["url"],
-                          fit: BoxFit.cover,
-                        )
-                    ),
-                  ),
-                ),
-                SliverList(
-                    delegate: SliverChildListDelegate([
-                      SingleChildScrollView(
-                          child: Container(
-                              padding: EdgeInsets.all(16),
-                              child: Column(
-                                children: <Widget>[
-                                  Container(
-                                    child: Row(
-                                      children: <Widget>[
-                                        Icon(
-                                          Icons.access_time,
-                                          color: Colors.grey,
-                                        ),
-                                        Padding(
-                                            padding: EdgeInsets.fromLTRB(0, 0, 6, 0)
-                                        ),
-                                        Text(
-                                          time,
-                                          style: TextStyle(
-                                              color: Colors.grey
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                  Padding(
-                                      padding: EdgeInsets.fromLTRB(0, 10, 0, 0)
-                                  ),
-                                  Text(
-                                    widget.post.data["title"],
-                                    style: TextStyle(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 180.0,
+              floating: true,
+              pinned: false,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Hero(
+                    tag: widget.post.documentID,
+                    child: Image.network(
+                      widget.post.data["url"],
+                      fit: BoxFit.cover,
+                    )),
+              ),
+            ),
+            SliverList(
+                delegate: SliverChildListDelegate([
+              SingleChildScrollView(
+                  child: Container(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.access_time,
+                                  color: Colors.grey,
+                                ),
+                                Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 0, 6, 0)),
+                                Text(
+                                  time,
+                                  style: TextStyle(color: Colors.grey),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0)),
+                          Text(
+                            widget.post.data["title"],
+                            style: TextStyle(
 //                      fontWeight: FontWeight.w600,
-                                      fontSize: 25,
-                                      fontFamily: 'NanumSquareRound',
-
-                                    ),
-                                  ),
-                                  Padding(
-                                      padding: EdgeInsets.fromLTRB(0, 20, 0, 0)
-                                  ),
-                                  Text(
-                                      contents,
-                                    style: TextStyle(
-                                      fontFamily: 'NanumSquareRound',
-                                      fontSize: 17,
-                                      height: 1.5
-                                    ),
-                                  ),
-                                  Padding(
-                                      padding: EdgeInsets.fromLTRB(0, 50, 0, 0)
-                                  ),
-                                  ListView.builder(
-                                    scrollDirection: Axis.vertical,
-                                    shrinkWrap: true,
-                                    padding: const EdgeInsets.all(8),
-                                    itemCount: comments_data.length,
-                                    itemBuilder: (BuildContext context, int index){
-                                      return ListTile(
-                                        title: Text(comments_data[index]),
-                                      );
-                                    },
-
-                                  )
+                              fontSize: 25,
+                              fontFamily: 'NanumSquareRound',
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0)),
+                          Text(
+                            contents,
+                            style: TextStyle(
+                                fontFamily: 'NanumSquareRound',
+                                fontSize: 17,
+                                height: 1.5),
+                          ),
+                          Padding(padding: EdgeInsets.fromLTRB(0, 50, 0, 0)),
+                          write_comments(),
+                          ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            padding: const EdgeInsets.all(8),
+                            itemCount: comments_data.length,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemBuilder: (BuildContext context, int index) {
+                              return ListTile(
+                                title: Text(comments_data[index]),
+                              );
+                            },
+                          )
 //                                   ListView.builder(
 //                                     padding: const EdgeInsets.all(8),
 //                                        itemCount: comments_data.length,
@@ -140,19 +171,60 @@ class articlepageState extends State<articlepage>{
 //                                        );
 //                                      },
 //                                   )
-  
-                                ],
-                              )
-                          )
-                      )
-                    ])
-
-                )
-              ],
-
-
+                        ],
+                      )))
+            ]))
+          ],
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Add your onPressed code here!
+          FocusScope.of(context).requestFocus(myFocusNode);
+          setState(() {
+            if (comment_state == false) {
+              comment_state = true;
+            } else {
+              comment_state = false;
+            }
+          });
+        },
+        label: Text(message),
+        icon: Icon(Icons.comment),
+        backgroundColor: Colors.lightGreen,
+      ),
     );
+  }
+
+  refresh() async {
+    DocumentReference getcomment =
+        Firestore.instance.collection('news').document(widget.post.documentID);
+    DocumentSnapshot doc = await getcomment.get();
+    setState(() {
+      print(comments_data);
+      comments_data = doc.data['comments'];
+      print(comments_data);
+    });
+
+    print(comments_data);
+  }
+
+  _onSave() async {
+    final form = _formKey.currentState;
+
+    if (!form.validate()) {
+      return;
+    }
+
+    DocumentReference getcomment =
+        Firestore.instance.collection('news').document(widget.post.documentID);
+
+    List<String> cmt = [commenttmp];
+
+    getcomment.updateData({'comments': FieldValue.arrayUnion(cmt)});
+    setState(() {
+      refresh();
+      comments_data;
+    });
   }
 }
