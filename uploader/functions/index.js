6cb6062c8da99b8b771b488eacff42394e39be6e
Roh Geonho <roh517@naver.com>
2020-04-01 17:00:38 +0900

    web stuff

--- uploader/functions/index.js
+++ uploader/functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const engines = require('consolidate');
+const firebase = require('firebase-admin');
+
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+
+const app = express();
+
+var bodyParser = require('body-parser');
+
+app.engine('hbs', engines.handlebars);
+app.set('views','./views');
+app.set('view engine', 'hbs');
+
+app.get('/',(request, response)=>{
+    response.set('Cache-Control','public, max-age=300, s-maxage=600');
+    response.render('index',{layout: 'default', template: 'home-template'});
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+exports.app = functions.https.onRequest(app);
+
